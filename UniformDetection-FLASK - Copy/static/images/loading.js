07a98408dda4fc8ff32d1f513f9da15529fff2d5
Kevin Joseph Hermonio <119944401+HermonioKJ@users.noreply.github.com>
2024-07-02 23:22:55 +0800

    Delete UniformDetection-FLASK - Copy directory

--- UniformDetection-FLASK - Copy/static/images/loading.js
+++
@@ -1,9 +0,0 @@
-// loading.js
-document.addEventListener('DOMContentLoaded', function () {
-    const form = document.querySelector('form');
-    const loadingOverlay = document.getElementById('loading-overlay');
-
-    form.addEventListener('submit', function () {
-        loadingOverlay.style.display = 'block';
-    });
-});
